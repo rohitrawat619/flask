8ff57ba592d1ebf8c23157fd6b72cb510088bbd5
rohitrawat <rohitrawat676@gmail.comt>
2025-03-09 22:16:13 +0530

    cron to migration severket

--- sweden.py
+++ sweden.py
@@ -1,52 +1,29 @@
-# import smtplib
-# import schedule
-# import time
-
-# # Email credentials
-# EMAIL_SENDER = "rohitrawat676@gmail.com"
-# EMAIL_PASSWORD = "Sbi@2089676@"  # Use App Password for security
-# EMAIL_RECEIVER = "rohitrawat8126844298@gmail.com"
-
-# def send_email():
-#     subject = "Good Morning!"
-#     body = "Hey buddy, have a great day! 😊"
-
-#     email_message = f"Subject: {subject}\n\n{body}"
-
-#     try:
-#         with smtplib.SMTP("smtp.gmail.com", 587) as server:
-#             server.starttls()
-#             server.login(EMAIL_SENDER, EMAIL_PASSWORD)
-#             server.sendmail(EMAIL_SENDER, EMAIL_RECEIVER, email_message)
-#         print("Email sent successfully!")
-#     except Exception as e:
-#         print(f"Error: {e}")
-
-# send_email()
-
-
-# # Schedule the email every morning at 8 AM
-# schedule.every().day.at("08:00").do(send_email)
-
-# print("Scheduler started...")
-
-# while True:
-#     schedule.run_pending()
-#     time.sleep(60)  # Check every minute
-
-
 import smtplib
 import schedule
 import time
 
 # Email credentials
 EMAIL_SENDER = "rohitrawat676@gmail.com"
-EMAIL_PASSWORD = "Sbi@2089676@"  # Use App Password for security
-EMAIL_RECEIVER = "rohitrawat8126844298@gmail.com"
+EMAIL_PASSWORD = "zkgdqjvyngyihsge"  # Use App Password for security
+EMAIL_RECEIVERS = [
+    "migrationsverket@migrationsverket.se",
+    "culture.stockholm@mea.gov.in",
+    "support-Sweden@ustraveldocs.com",
+    "hoc.stockholm@mea.gov.in"
+    "stockholm-arbetstillstandsenhet3@migrationsverket.se",
+    "arbetstillstandsenhet3@migrationsverket.se",
+    "rohitrawat8126844298@gmail.com",
+    "sandra.nunez@migrationsverket.se"
+    ]
 
 def send_email():
-    subject = "Good Morning!"
-    body = "Hey buddy, have a great day! 😊"
+    subject = "Request Regarding Refund My Resident Permit Visa fees || Control no 62151395"
+    body = """\
+        Dear Sandra Nunez,
+        Be assured of the withdrawal of my application in a full refund of visa fees condition only that is Euro 210. 
+        Kindly, withdraw my application with control no 62151395 & refund visa fees to my bank account ICIC A/C - 016401026756 & IFSC Code - ICIC0000164.
+        Best regards,
+        """
 
     email_message = f"Subject: {subject}\n\n{body}"
 
@@ -54,19 +31,27 @@ def send_email():
         with smtplib.SMTP("smtp.gmail.com", 587) as server:
             server.starttls()
             server.login(EMAIL_SENDER, EMAIL_PASSWORD)
-            server.sendmail(EMAIL_SENDER, EMAIL_RECEIVER, email_message)
+            server.sendmail(EMAIL_SENDER, EMAIL_RECEIVERS, email_message)
         print("Email sent successfully!")
     except Exception as e:
         print(f"Error: {e}")
 
-# Send email instantly on script start
 send_email()
 
 # Schedule the email every morning at 8 AM
-schedule.every().day.at("08:00").do(send_email)
+schedule.every().day.at("08:00").do(send_email)  # 8 AM
+schedule.every().day.at("11:00").do(send_email)  # 4 PM
+schedule.every().day.at("13:00").do(send_email)  # 8 AM
+schedule.every().day.at("15:00").do(send_email)  # 5 PM
+schedule.every().day.at("16:00").do(send_email)  # 6 PM
+schedule.every().day.at("17:00").do(send_email)  # 7 AM
+schedule.every().day.at("20:00").do(send_email)  # 10 PM
 
 print("Scheduler started...")
 
 while True:
     schedule.run_pending()
     time.sleep(60)  # Check every minute
+
+
+
